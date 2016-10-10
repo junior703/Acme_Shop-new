5e9be53e8639e620bd31c04e1a745a40bda0e223
MACKENDY <MACKENDY@172.16.4.100>
2016-10-10 17:38:34 -0500

    models

--- Documents/NetBeansProjects/proyectoCompartido/src/models/ModelPerimetro.java
+++ Documents/NetBeansProjects/proyectoCompartido/src/models/ModelPerimetro.java
@@ -11,5 +11,125 @@ package models;
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class ModelPerimetro {
+private Double lado=0.0;
+private Double resultquadra=0.0;
 
+private Double ladoRombo=0.0;
+private Double resultRombo=0.0;
+
+private Double largo=0.0;
+private Double ancho=0.0;
+private Double resultRect=0.0;
+
+    /**
+     * @return the lado
+     */
+    public Double getLado() {
+        return lado;
+    }
+
+    /**
+     * @param lado the lado to set
+     */
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+    /**
+     * @return the resultquadra
+     */
+    public Double getResultquadra() {
+        return resultquadra;
+    }
+
+    /**
+     * @param resultquadra the resultquadra to set
+     */
+    public void setResultquadra(Double resultquadra) {
+        this.resultquadra = resultquadra;
+    }
+
+    /**
+     * @return the ladoRombo
+     */
+    public Double getLadoRombo() {
+        return ladoRombo;
+    }
+
+    /**
+     * @param ladoRombo the ladoRombo to set
+     */
+    public void setLadoRombo(Double ladoRombo) {
+        this.ladoRombo = ladoRombo;
+    }
+
+    /**
+     * @return the resultRombo
+     */
+    public Double getResultRombo() {
+        return resultRombo;
+    }
+
+    /**
+     * @param resultRombo the resultRombo to set
+     */
+    public void setResultRombo(Double resultRombo) {
+        this.resultRombo = resultRombo;
+    }
+
+    /**
+     * @return the largo
+     */
+    public Double getLargo() {
+        return largo;
+    }
+
+    /**
+     * @param largo the largo to set
+     */
+    public void setLargo(Double largo) {
+        this.largo = largo;
+    }
+
+    /**
+     * @return the ancho
+     */
+    public Double getAncho() {
+        return ancho;
+    }
+
+    /**
+     * @param ancho the ancho to set
+     */
+    public void setAncho(Double ancho) {
+        this.ancho = ancho;
+    }
+
+    /**
+     * @return the resultRect
+     */
+    public Double getResultRect() {
+        return resultRect;
+    }
+
+    /**
+     * @param resultRect the resultRect to set
+     */
+    public void setResultRect(Double resultRect) {
+        this.resultRect = resultRect;
+    }
+    
+      public double quadrado(){
+         resultquadra= lado*4; 
+    return resultquadra;
+    }
+    public double rombo(){
+   resultRombo= ladoRombo*4;
+    return resultRombo;
+    }
+    
+    public double rectangulo(){
+        resultRect=(largo+ancho)*2;
+    return resultRect;
+    }
 }
--- Documents/NetBeansProjects/proyectoCompartido/src/models/ModelVolumen.java
+++ Documents/NetBeansProjects/proyectoCompartido/src/models/ModelVolumen.java
@@ -11,5 +11,156 @@ package models;
  * @author Sammy Guergachi <sguergachi at gmail.com>
  */
 public class ModelVolumen {
+private Double lado=0.0;
+private Double resultquadra=0.0;
 
+private Double ladoRombo=0.0;
+private Double alturaRombo=0.0;
+private Double resultRombo=0.0;
+
+private Double alto=0.0;
+private Double largo=0.0;
+private Double ancho=0.0;
+private Double resultRect=0.0;
+
+    /**
+     * @return the lado
+     */
+    public Double getLado() {
+        return lado;
+    }
+
+    /**
+     * @param lado the lado to set
+     */
+    public void setLado(Double lado) {
+        this.lado = lado;
+    }
+
+    /**
+     * @return the resultquadra
+     */
+    public Double getResultquadra() {
+        return resultquadra;
+    }
+
+    /**
+     * @param resultquadra the resultquadra to set
+     */
+    public void setResultquadra(Double resultquadra) {
+        this.resultquadra = resultquadra;
+    }
+
+    /**
+     * @return the ladoRombo
+     */
+    public Double getLadoRombo() {
+        return ladoRombo;
+    }
+
+    /**
+     * @param ladoRombo the ladoRombo to set
+     */
+    public void setLadoRombo(Double ladoRombo) {
+        this.ladoRombo = ladoRombo;
+    }
+
+    /**
+     * @return the alturaRombo
+     */
+    public Double getAlturaRombo() {
+        return alturaRombo;
+    }
+
+    /**
+     * @param alturaRombo the alturaRombo to set
+     */
+    public void setAlturaRombo(Double alturaRombo) {
+        this.alturaRombo = alturaRombo;
+    }
+
+    /**
+     * @return the resultRombo
+     */
+    public Double getResultRombo() {
+        return resultRombo;
+    }
+
+    /**
+     * @param resultRombo the resultRombo to set
+     */
+    public void setResultRombo(Double resultRombo) {
+        this.resultRombo = resultRombo;
+    }
+
+    /**
+     * @return the largo
+     */
+    public Double getLargo() {
+        return largo;
+    }
+
+    /**
+     * @param largo the largo to set
+     */
+    public void setLargo(Double largo) {
+        this.largo = largo;
+    }
+
+    /**
+     * @return the ancho
+     */
+    public Double getAncho() {
+        return ancho;
+    }
+
+    /**
+     * @param ancho the ancho to set
+     */
+    public void setAncho(Double ancho) {
+        this.ancho = ancho;
+    }
+
+    /**
+     * @return the resultRect
+     */
+    public Double getResultRect() {
+        return resultRect;
+    }
+
+    /**
+     * @param resultRect the resultRect to set
+     */
+    public void setResultRect(Double resultRect) {
+        this.resultRect = resultRect;
+    }
+    
+    /**
+     * @return the alto
+     */
+    public Double getAlto() {
+        return alto;
+    }
+
+    /**
+     * @param alto the alto to set
+     */
+    public void setAlto(Double alto) {
+        this.alto = alto;
+    }
+    
+          public double quadrado(){
+         resultquadra= lado*lado*lado; 
+    return resultquadra;
+    }
+    public double rombo(){
+   resultRombo= 0.33*ladoRombo*ladoRombo/alturaRombo;
+    return resultRombo;
+    }
+    
+    public double rectangulo(){
+        resultRect=alto* largo*ancho;
+    return resultRect;
+    }
 }
+
